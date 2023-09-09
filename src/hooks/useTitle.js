2b0632c583f66678691a31415be3744ca98d6806
Mohd Aazib <rahil.aazib@gmail.com>
2023-09-10 00:24:20 +0530

    title updated

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export  function useTitle(title) {
 
 useEffect(()=>{
-    document.title=`${title} | Shop-Mate`;
+    document.title=`${title} | Shop-Mate-Redux`;
 },[title])
 
   return null;
